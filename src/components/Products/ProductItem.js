dd8dcec50ec19810fb777b6997d4782dc378efab
Angélique Cléry <angelique.clery@DST1013.local>
2019-10-28 14:23:06 +0100

    test ie

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -6,6 +6,7 @@ import { devENVSetCoupons } from '../../utils/index';
 import './Products.css';
 
 const ProductItem = ({ item, index, user, tracking }) => {
+  console.log('test ie')
   // récupération des infos du user via Jahia:
   const clubMember = user.isMember;
   const clubRank = user.priceClubRank;
