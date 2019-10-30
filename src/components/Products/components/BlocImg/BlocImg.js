40e612b6f96bf3bf64b922f69698f11cb510133a
Angélique Cléry <angelique.clery@DST1013.local>
2019-10-30 16:07:31 +0100

    adding react-lazyload

--- src/components/Products/components/BlocImg/BlocImg.js
+++ src/components/Products/components/BlocImg/BlocImg.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './BlocImg.css';
+import Lazyload from 'react-lazyload';
+import { TransitionGroup } from 'react-transition-group';
 
 function BlocImg(props) {
     const { data } = props;
-  
+
   return (
     <div className="flux-promo-item-picture relative">
         <div className="flex flex-column items-center">
-            <img className="flux-promo-item-img center" src={data.imgUrl} alt={data.titleLimited} />
+            <Lazyload throttle={250} height={200} once>
+                <TransitionGroup key="1"
+                 transitionName="fade"
+                 transitionAppear
+                 transitionAppearTimeout={500}
+                 transitionEnter={false}
+                 transitionLeave={false}>
+                    <img className="flux-promo-item-img center" src={data.imgUrl} alt={data.titleLimited} />
+                </TransitionGroup>
+            </Lazyload>
             <div className="helper_hide-BelowTablet fixed-height-20">
                 { data.noteRounded > 0 ?
                     <p className={`flex items-center rating_${data.noteRoundedClass}`}>
