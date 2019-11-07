508c99b04a0ad15e100c35a3db2653b1dc6fd7ab
Angélique Cléry <angelique.clery@DST1013.local>
2019-11-07 10:56:19 +0100

    fix affichage 0%

--- src/components/Products/components/BlocPrice/Components/PromoPrice.js
+++ src/components/Products/components/BlocPrice/Components/PromoPrice.js
@@ -10,11 +10,23 @@ function PromoPrice(props) {
       return economy.toLocaleString('fr-FR', { maximumFractionDigits: 2 })
     }
     let priceCase = '';
-    if ( data.originalPrice && data.originalPrice > data.price || data.refPrice && data.refPrice > data.price) {
+    if ( data.originalPrice && data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ) {
       priceCase = 'isPromo'
     }
-    if ( !data.originalPrice && !data.refPrice || data.originalPrice <= data.price ) {
+    if ( !data.originalPrice && data.refPrice && data.refPrice > data.price && data.pctDiscount > 1 ) {
+      priceCase = 'isPromo'
+    }
+    if ( !data.originalPrice && !data.refPrice ) {
       priceCase = 'noPromo'
+      console.log(priceCase)
+    }
+    if ( data.originalPrice && discountOriginalPrice(data.price,data.originalPrice) <= 1 || data.originalPrice <= data.price ) {
+      priceCase = 'noPromo'
+      console.log(priceCase)
+    }
+    if ( !data.originalPrice && data.pctDiscount <= 1 || !data.originalPrice && data.refPrice <= data.price ) {
+      priceCase = 'noPromo'
+      console.log(priceCase)
     }
 
   return (
@@ -24,12 +36,12 @@ function PromoPrice(props) {
     <div className="flux-promo-blocPrice-promo flex flex-column mt1 mb3">
       <div className="flex justify-center pt2 fixed-height-21">
         <p className="flux-promo-price-off dib line-through f14 pr2">
-        { data.originalPrice > data.price ? <span>{formatedPrice(data.originalPrice)} &#8364;</span>
-            : data.refPrice > data.price ? <span>{formatedPrice(data.refPrice)} &#8364;</span>
+        { data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? <span>{formatedPrice(data.originalPrice)} &#8364;</span>
+            : data.refPrice > data.price && data.pctDiscount > 1 ? <span>{formatedPrice(data.refPrice)} &#8364;</span>
             : '' 
         }
         </p>
-        { data.originalPrice > data.price ? <span className="dib white b f12 flux-promo-pct-discount">-{discountOriginalPrice(data.price,data.originalPrice)}%</span> 
+        { data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? <span className="dib white b f12 flux-promo-pct-discount">-{discountOriginalPrice(data.price,data.originalPrice)}%</span> 
           : !data.originalPrice && data.refPrice && data.pctDiscount > 1 ? <span className="dib white b f12 flux-promo-pct-discount">-{data.pctDiscount}%</span> 
           : ''
           }
@@ -38,13 +50,13 @@ function PromoPrice(props) {
           <p className="f54 dib">{Math.trunc(data.price)}<sup className="f22 price-euro-full">€</sup></p>
           { getFraction(data.price) !== ',00' ? <p className="price-decimal-full_large-device dib f22">{getFraction(data.price)}</p> : '' }
       </div>
-        { data.originalPrice && data.originalPrice > data.price ? 
+        { data.originalPrice && data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? 
       <div className="promo-price-economy dib center f14 black bg-lightgray ph3 pv2 mt2">
         <div className="promo-price-economy-text dib">
           <p>Soit<span className="b"> {economyCalcul(data.originalPrice,data.price)} &#8364;</span> d'économie</p>
         </div>
       </div>
-        : data.refPrice ?   
+        : data.refPrice && data.pctDiscount > 1 ?   
         <div className="promo-price-economy dib center f14 black bg-lightgray ph3 pv2 mt2">
           <div className="promo-price-economy-text dib">
             <p>Soit<span className="b"> {economyCalcul(data.refPrice,data.price)} &#8364;</span> d'économie</p>
@@ -59,12 +71,12 @@ function PromoPrice(props) {
     <div className="flux-promo-blocPrice-promo_small-device flex flex-column mt3">
       <div className="flex justify-center pt3 pb2">
         <p className="flux-promo-price-off dib line-through f14 pr2">
-          { data.originalPrice > data.price ? <span>{formatedPrice(data.originalPrice)} &#8364;</span>
-            : data.refPrice > data.price ? <span>{formatedPrice(data.refPrice)} &#8364;</span>
+          { data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? <span>{formatedPrice(data.originalPrice)} &#8364;</span>
+            : data.refPrice > data.price && data.pctDiscount > 1 ? <span>{formatedPrice(data.refPrice)} &#8364;</span>
             : <span className="fixed-height-15"></span> 
           }
         </p>
-        { data.originalPrice > data.price ? <span className="dib white b f10 flux-promo-pct-discount">-{discountOriginalPrice(data.price,data.originalPrice)}%</span> 
+        { data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? <span className="dib white b f10 flux-promo-pct-discount">-{discountOriginalPrice(data.price,data.originalPrice)}%</span> 
             : !data.originalPrice && data.refPrice && data.pctDiscount > 1 ? <span className="dib white b f10 flux-promo-pct-discount">-{data.pctDiscount}%</span> 
             : <span className="fixed-height-15"></span> 
           }
@@ -73,13 +85,13 @@ function PromoPrice(props) {
           <p className="f34 dib">{Math.trunc(data.price)}<sup className="f16 price-euro-full_small-device">€</sup></p>
           { getFraction(data.price) !== ',00' ? <p className="price-decimal-full_small-device dib f16">{getFraction(data.price)}</p> : '' }
       </div>
-      { data.originalPrice && data.originalPrice > data.price ? 
+      { data.originalPrice && data.originalPrice > data.price && discountOriginalPrice(data.price,data.originalPrice) > 1 ? 
         <div className="promo-price-economy dib center f12 black bg-lightgray ph3 pv2 mt1">
           <div className="promo-price-economy-text dib">
             <p>Soit<span className="b"> {economyCalcul(data.originalPrice,data.price)} &#8364;</span> d'économie</p>
           </div>
         </div>
-        : data.refPrice ?   
+        : data.refPrice && data.pctDiscount > 1 ?   
         <div className="promo-price-economy dib center f12 black bg-lightgray ph3 pv2 mt1">
           <div className="promo-price-economy-text dib">
             <p>Soit<span className="b"> {economyCalcul(data.refPrice,data.price)} &#8364;</span> d'économie</p>
